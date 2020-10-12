0e6b8c804f1dad02921d4a672df4f2e3340b73b1
TEJASH D P <55584501+tejash9449@users.noreply.github.com>
2020-10-12 17:40:16 +0530

    LetsUpgrade-java ASSIGNMENT-1
    Create a class employee. Inside that class create three variable names, age.city. Also, create a function to display the three variables.
     In the main function create two objects and call the function using it
    
    Output:
    The name is Saurab
    The age is 23
    The city is chennai

---
+++ company/Main.java
@@ -0,0 +1,20 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	 employee e1=new employee();
+	 e1.name="Pranesh";
+	 e1.age=20;
+	 e1.city="Mysore";
+	 e1.Display();
+		
+
+
+	 employee e2=new employee();
+	 e2.name="Nitin.T";
+	 e2.age=21;
+	 e2.city="Bangalore";
+	 e2.Display();
+    }
+}
---
+++ company/employee.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class employee {
+    public String name;
+    public int age;
+    public String city;
+    public void Display(){
+        System.out.println("The name is " + name);
+        System.out.println("The age is " + age);
+        System.out.println("The city is " + city);
+        System.out.println("%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%");
+    }
+}
